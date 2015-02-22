0e16aeee4028557f08b25d8bbe6eb7d98ede9832
hiroo takaku <slgerkamp@gmail.com>
2015-02-22 10:40:40 +0900

    chapter03：複数のプロパティの比較

---
+++ Functional-Programing-in-Java/src/test/java/com/slgerkamp/fpij/chapter03/MultipleAndFluentComparisonsTest.java
@@ -0,0 +1,102 @@
+package com.slgerkamp.fpij.chapter03;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Comparator.*;
+
+import org.junit.Test;
+
+/**
+ * 3.3 複数のプロパティによる流暢な比較
+ *
+ */
+public class MultipleAndFluentComparisonsTest {
+	
+	// テスト対象リスト
+	final List<Person> people = Arrays.asList(
+			new Person("Sara", 21),
+			new Person("Jane", 21),
+			new Person("John", 20),
+			new Person("Greg", 35)
+		);
+	
+	// 名前でソートした期待リスト
+	final List<Person> nameSortedexpected = Arrays.asList(
+			new Person("Greg", 35),
+			new Person("Jane", 21),
+			new Person("John", 20),
+			new Person("Sara", 21)
+		);
+
+	// 名前と年齢でソートした期待リスト
+	final List<Person> ageAndNameSortedexpected = Arrays.asList(
+			new Person("John", 20),
+			new Person("Jane", 21),
+			new Person("Sara", 21),
+			new Person("Greg", 35)
+		);
+
+	
+	@Test
+	public void 名前でソートする_内部クラス構文(){
+
+		List<Person> nameSortedList = people
+			.stream()
+			.sorted((person1, person2) -> person1.name.compareTo(person2.name))
+			.collect(toList());
+		
+		assertHelper(nameSortedList, nameSortedexpected);
+		
+	}
+	
+	@Test
+	public void 名前でソートする_コンビニエント関数(){
+
+		final Function<Person, String> byName = person -> person.name;
+		
+		// Comparatorのコンビニエント関数
+		// comparingは与えられたラムダ式からComparatorインタフェースを作成
+		List<Person> nameSortedList = people
+			.stream()
+			.sorted(comparing(byName))
+			.collect(toList());
+		
+		assertHelper(nameSortedList, nameSortedexpected);
+	}
+	
+	@Test
+	public void 年齢と名前でソートする_コンビニエント関数(){
+
+		final Function<Person, Integer> byAge  = person -> person.age;
+		final Function<Person, String> byName = person -> person.name;
+		
+		List<Person> ageAndNameSortedList = people
+			.stream()
+			.sorted(comparing(byAge).thenComparing(byName))
+			.collect(toList());
+		
+		assertHelper(ageAndNameSortedList, ageAndNameSortedexpected);
+	}
+
+	/**
+	 * comparatorがないので比較ができない
+	 * かなり悲しいforループを使用
+	 * 名前は重複していない前提で名前での確認
+	 * @param actual
+	 * @param expected
+	 */
+	private void assertHelper(List<Person> actual, List<Person> expected){
+		for(int i = 0; i < actual.size(); i++){
+			String sortedItem = actual.get(i).name;
+			String expectedItem = expected.get(i).name;
+			assertThat(sortedItem, is(expectedItem));				
+		}
+		
+	}
+}
