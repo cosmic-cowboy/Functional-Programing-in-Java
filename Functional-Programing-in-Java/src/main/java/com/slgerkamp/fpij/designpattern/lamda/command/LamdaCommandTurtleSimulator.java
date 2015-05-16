7504c05732f71fc059f6efca7de2cd33267d8598
hiroo takaku <slgerkamp@gmail.com>
2015-05-16 12:56:13 +0900

    ラムダ式でデザインパターン（ラムダ式を使ったcommandパターン）：Commandの実装をラムダ式で実現

---
+++ Functional-Programing-in-Java/src/main/java/com/slgerkamp/fpij/designpattern/lamda/command/LamdaCommandTurtleSimulator.java
@@ -0,0 +1,34 @@
+package com.slgerkamp.fpij.designpattern.lamda.command;
+
+import com.slgerkamp.fpij.designpattern.nonlamda.command.invoker.TurtleMover;
+import com.slgerkamp.fpij.designpattern.nonlamda.command.receiver.Turtle;
+
+public class LamdaCommandTurtleSimulator {
+
+	public static void main(String[] args) {
+		
+        String userInput = "↓←←↑↑→→→→↓↓";
+
+        /*
+         * 亀の動作を司る（登録されたコマンドオブジェクトを用いて動作を起動するInvoker）に
+         * 亀の動作（動作を表すCommand）を登録する
+         * commandはラムダ式で作成
+         */
+        TurtleMover turtleMover = new TurtleMover();
+        turtleMover.registerMotion('←', t -> t.setX(t.getX() - 1));
+        turtleMover.registerMotion('↓',  t -> t.setY(t.getY() - 1));
+        turtleMover.registerMotion('↑',  t -> t.setY(t.getY() + 1));
+        turtleMover.registerMotion('→',  t -> t.setX(t.getX() + 1));
+
+		// 亀（動作の対象Receiver）を生成
+        Turtle turtle = new Turtle(0, 0);
+
+		// 亀を動作させる
+        turtleMover.moveAround(turtle, userInput);
+
+        // (5) 移動先を表示
+        System.out.printf("X=%d Y=%d%n", turtle.getX(), turtle.getY());
+		
+		
+	}
+}
